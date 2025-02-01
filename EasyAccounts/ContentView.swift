c5cbb95085a9bb964f8e244cf4a2ea925f26ba2e
rockyshen <875430544@qq.com>
2025-02-01 16:31:16 +0800

    Initial Commit

---
+++ EasyAccounts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EasyAccounts
+//
+//  Created by 沈俊杰 on 2025/2/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EasyAccounts/EasyAccountsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyAccountsApp.swift
+//  EasyAccounts
+//
+//  Created by 沈俊杰 on 2025/2/1.
+//
+
+import SwiftUI
+
+@main
+struct EasyAccountsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
