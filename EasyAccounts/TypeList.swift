5bfe2f4417128fca43aa1940088689eaa5d6440d
rockyshen <875430544@qq.com>
2025-02-11 10:12:00 +0800

    feature：完成“分类页”的TypeView和TypeList的开发；完成TypeStore数据实体类的定义；修复AddFlowView中动态获取Type的TODO。

---
+++ EasyAccounts/TypeList.swift
@@ -0,0 +1,18 @@
+//
+//  TypeList.swift
+//  EasyAccounts
+//
+//  Created by 沈俊杰 on 2025/2/11.
+//
+
+import SwiftUI
+
+struct TypeList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TypeList()
+}
---
+++ EasyAccounts/model/TypeStore.swift
@@ -0,0 +1,8 @@
+//
+//  TypeStore.swift
+//  EasyAccounts
+//
+//  Created by 沈俊杰 on 2025/2/11.
+//
+
+import Foundation
